5d47fc8c494593c639158eee68793983896e60a3
Alexey Krutyakov <coder.arctik@gmail.com>
2023-08-15 21:28:18 +0300

    refactor: onClick function in Tab component

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -4,43 +4,35 @@ import styles from './tabs.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import scroll from '../../utils/scroll';
 
+
 function Tabs() {
   const [current, setCurrent] = React.useState('buns');
+  
+  function setCategory(value) {
+    setCurrent(value);
+    scroll(value);
+  }
+  
   return (
     <nav className={`${styles.tabs} mt-5`} style={{ display: 'flex' }}>
       <Tab
         value='buns'
         active={current === 'buns'}
-        onClick={
-          () => {
-            setCurrent('buns');
-            scroll('buns');
-          }
-        }
+        onClick={setCategory}
       >
         Булки
       </Tab>
       <Tab
         value='sauces'
         active={current === 'sauces'}
-        onClick={
-          () => {
-            setCurrent('sauces');
-            scroll('sauces');
-          }
-        }
+        onClick={setCategory}
       >
         Соусы
       </Tab>
       <Tab
         value='mains'
         active={current === 'mains'}
-        onClick={
-          () => {
-            setCurrent('mains');
-            scroll('mains');
-          }
-        }
+        onClick={setCategory}
       >
         Начинки
       </Tab>
