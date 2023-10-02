3ab75501b8dfa2b45bc24a0230c7241ea2fe26a4
Alexey Krutyakov <coder.arctik@gmail.com>
2023-10-02 21:51:28 +0300

    feat: add pageNotFound_404

--- src/pages/not-found/not-found.js
+++ src/pages/not-found/not-found.js
@@ -1,4 +1,7 @@
+import styles from './not-found.module.css';
 import { useEffect } from "react";
+import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useNavigate } from 'react-router';
 
 export default function NotFound404() {
 
@@ -6,7 +9,21 @@ export default function NotFound404() {
     document.title = 'Stellar Burgers: Not found 404';
   });
 
+  const navigate = useNavigate();
+
+  function clickHandler() {
+    navigate('/');
+  };
+
   return (
-    <h1>Страница не найдена</h1>
+    <section class={styles.container}>
+		  <div className={styles.wrapper}>
+			  <article className={styles.content}>
+				  <h1 className={`${styles.request_code} text text_type_digits-large`}>404</h1>
+				  <p className={`${styles.request_text} text text_type_main-large`}>Страница&nbsp;не&nbsp;найдена</p>
+			  </article>
+			  <Button htmlType='button' type='primary' size='medium' extraClass='mt-6' onClick={clickHandler}>На главную</Button>
+		  </div>
+	  </section>
   );
 }
