91541c7a64fae1f1afc557ce20bf200c24a7be85
Alexey Krutyakov <coder.arctik@gmail.com>
2023-12-09 19:22:32 +0300

    fix: add request new access token in middleWare

--- src/services/socketMiddleWare.ts
+++ src/services/socketMiddleWare.ts
@@ -1,6 +1,8 @@
 import { Middleware } from 'redux';
 // import types
 import { RootState, WsConfig } from 'types';
+import { requestNewTokens } from 'utils/api';
+import { API_URLS, TOKENS, WS_ACTIONS } from 'utils/constants';
 
 export const socketMiddleware = (
   wsActions: WsConfig,
@@ -29,6 +31,22 @@ export const socketMiddleware = (
         };
         socket.onmessage = event => {
           const data = JSON.parse(event.data);
+          if (data.message === 'Invalid or missing token') {
+            requestNewTokens()
+              .then(res => {
+                localStorage.setItem(TOKENS.names.refresh, res.refreshToken);
+                localStorage.setItem(TOKENS.names.access, res.accessToken);
+              })
+              .then(() => {
+                const token = localStorage.getItem(TOKENS.names.access);
+                dispatch({
+                  type: WS_ACTIONS.ordersWsInit,
+                  payload: `${API_URLS.wss.personalOrders}?token=${
+                    token ? token.split('Bearer ')[1] : ''
+                  }`,
+                });
+              });
+          }
           dispatch(onMessage(data));
         };
         socket.onclose = event => {
